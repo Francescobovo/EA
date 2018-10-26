71c56aa4c1a081484d9cef457d7c4fc9b326e28c
Gideon Hanse <Gideon@MacBook-Air-van-Gideon.local>
2018-10-26 18:37:33 +0200

    Deleted old files

--- src/GA.m
+++ src/GA.m
@@ -1,28 +1,53 @@
-#generating pop
-population = zeros(15,100);
-for i = 1:columns(population)
-population(:,i) = randi([1,25],[15,1]);
-valid_119(population(:,i))
+addpath matpower4.1/
+load('para119.mat')
+functionevals = 0;
+
+#selecting mates function
+function parents = selectparents(candidates)
+  losses = [];
+  for i = 1:columns(candidates)
+    losses = [losses, calculation_119(candidates(:,i))];
+  end
+  [S,I] = sort(losses(:));
+  parent1 = candidates(:,I(1));
+  parent2 = candidates(:,I(2));
+  parents = [parent1, parent2];
 end
 
 #crossover function
-function crossover = crossover(mate1, mate2, pc)
-
+function co_offspring = crossover(parents, pc)
+  co_offspring = zeros(15,1);
+  co_point = 7;
+  co_offspring(1:co_point) = parents(1:co_point, 2);
+  co_offspring(co_point+1:15) = parents(co_point+1:15, 1);
 end
 
-
-load('para119.mat');
-addpath matpower4.1
-functionevals = 0;
-
-#pseudocode selection
-for every individual in population
-  check feasibility
-  if feasible
+#
 
 
-while (functionevals < eval_budget)
+#create random population
+initial = [25;17;15;21;18;12;11;16;12;17;20;18;12;16;20];
 
-  functionevals += 1
+population = zeros(15,200);
+for i = 1:columns(population)
+  for j = 1:rows(population)
+    population(j,i) = randi([1 initial(j)]);
+  end
+end
 
-endwhile
+#validate
+candidates = [];
+for i = 1:columns(population)
+  if valid_119(population(:,i)) == 1
+    candidates = [candidates, population(:,i)];
+  end
+  functionevals++;
+end
+
+#begin GA
+
+#selecting parents
+parents = selectparents(candidates)
+
+#crossover
+
