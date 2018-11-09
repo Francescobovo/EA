95c597ae27288fdb290484f05465c80cb851a71b
Francescobovo <44617001+Francescobovo@users.noreply.github.com>
2018-11-09 10:34:03 +0100

    Update monte_carlo.m

--- src/monte_carlo.m
+++ src/monte_carlo.m
@@ -1,1 +1,32 @@
-function [aopt, fopt] = monte_carlo(f)  n = 133;  iters = 10000;  aopt = rand(1,n) > 0.5;  fopt= f(aopt);  for i = 2:iters    a = rand(1,n) > 0.5;    fa = f(a);    if(fa >= fopt)      aopt = a;      fopt = fa;    end  end  end
+function [aopt, fopt] = montecarlo(eval_budget)
+
+    addpath(genpath('/Users/francesco/Documents/Octave/source/matpower')); % use all subdirectories(including matpower)
+    load('para119.mat') % load the dataset
+
+    upper_b = para.ub; % take the upper bound and test whether the base topology is valid
+    lower_b = para.lb; % take the lower bound and test whether the base topology is valid
+    size = para.n;
+    vector = zeros(size, 1);
+    ftn = repmat(0, eval_budget);
+    flag = 0; 
+    % create valid individual
+    parfor i = 1:eval_budget
+      while flag == 0  
+        for j = 1:size % create an individual as random vector from lower bound to uppper bound
+          vector(j) = randi([lower_b(j), upper_b(j)]); 
+        end
+        if valid_119(vector) == 1 % stop loop if individual is valid
+          flag = 1;
+        end
+      end
+      ftn(i) = calculation_119(vector);
+      if i > 1 && ftn(i) > ftn(i-1)
+        ftn(i) = ftn(i-1);      % take the previous fitness if better
+      end
+    end
+    fopt = min(ftn);
+    aopt = vector;
+
+end
+
+
